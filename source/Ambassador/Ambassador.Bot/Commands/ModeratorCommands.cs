275944be621175af5ca8da3a3413f7bc831136a2
maratmuginov <mmuginov@protonmail.com>
2021-06-12 21:08:27 -0700

    Sweep command

---
+++ source/Ambassador/Ambassador.Bot/Commands/ModeratorCommands.cs
@@ -0,0 +1,26 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using System;
+using System.Threading.Tasks;
+
+namespace Ambassador.Bot.Commands
+{
+    public class ModeratorCommands : BaseCommandModule
+    {
+        [Command("Sweep")]
+        public async Task SweepAsync(CommandContext context, int messageCount)
+        {
+            if (messageCount < 1)
+            {
+                await context.Message.RespondAsync($"{nameof(messageCount)} cannot be less than 1");
+                return;
+            }
+
+            var messages = await context.Channel.GetMessagesAsync(messageCount);
+            await context.Channel.DeleteMessagesAsync(messages);
+            var confirmationMessage = await context.Channel.SendMessageAsync($"Deleted {messages.Count} messages.");
+            await Task.Delay(TimeSpan.FromSeconds(5));
+            await confirmationMessage.DeleteAsync();
+        }
+    }
+}
